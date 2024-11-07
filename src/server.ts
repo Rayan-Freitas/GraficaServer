f512e6e65a06574a40034510c3a1085b8186520f
Rayan-Freitas <rayan.freiitas@gmail.com>
2024-11-06 22:07:07 -0300

    Adicionando CORS

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { authRoutes } from './routes/authRoutes';
 import { userRoutes } from './routes/userRoutes';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());  // Para interpretar JSON no corpo da requisição
 
 // Middleware de autenticação JWT
